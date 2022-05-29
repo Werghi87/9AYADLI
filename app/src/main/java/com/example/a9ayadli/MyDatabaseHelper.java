2690a36c548c27a9f1951a2613f9a68af7ee8a87
Ibrahim Doghri <patron.bara@gmail.com>
2022-05-29 21:59:38 +0100

    Added quality of life features & revamped design just a bit

--- app/src/main/java/com/example/a9ayadli/MyDatabaseHelper.java
+++ app/src/main/java/com/example/a9ayadli/MyDatabaseHelper.java
@@ -105,6 +105,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+
     void deleteAllData(){
         SQLiteDatabase db = this.getWritableDatabase();
         db.execSQL("DELETE FROM " + TABLE_NAME);
